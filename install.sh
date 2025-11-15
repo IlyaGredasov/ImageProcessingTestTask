5f1499c168d078831fd567597add785af47a05fb
IlyaGredasov <iljagredasov@yandex.ru>
2025-11-15 16:12:22 +0300

    refactor: refactor install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-sudo apt install build-essential cmake ninja-build qt6-base-dev libgtest-dev
-sudo apt install libxkbcommon-dev
-sudo apt install qt6-tools-dev qt6-tools-dev-tools
+sudo apt install build-essential cmake ninja-build qt6-base-dev qt6-tools-dev qt6-tools-dev-tools libgtest-dev libxkbcommon-dev
